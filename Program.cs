473a1b84490a70988f7434a54e16d097b6cddbf4
Juliana Oliveira <134332673+JuhOliveira1202@users.noreply.github.com>
2023-07-17 11:59:40 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    public static int LerNumero()
+    {
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    public static float Converter(float a)
+    {
+        a = (-32) / 1.8F;
+        return a;
+    }
+
+    public static void Escrever(float valor)
+    {
+        Console.WriteLine(valor);
+    }
+
+    public static void Main(string[] args)
+    {
+        float valor;
+        valor = LerNumero();
+        valor = Converter(valor);
+        Escrever(valor);
+    }
+}
